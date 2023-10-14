72890b1aa6158aad4323edb8c1dbc1ee08cb6f0f
Wataru9029 <43335573+Wataru9029@users.noreply.github.com>
2023-10-14 12:42:22 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset_session      # ログインの直前に必ずこれを書くこと
+      reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      remember user
       log_in user
       redirect_to user
     else
@@ -15,11 +17,13 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-      log_out
+      log_out if logged_in?
       redirect_to root_url, status: :see_other
     end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url, status: :see_other
   end
 end
